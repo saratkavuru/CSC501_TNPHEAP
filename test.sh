b6a0b3a1c844904e88d551ccab77a4b1d57335ab
skavuru <skavuru@ncsu.edu>
2017-10-28 15:02:20 -0400

    missed this in previous commit

--- test.sh
+++ test.sh
@@ -3,10 +3,10 @@ sudo insmod NPHeap/npheap.ko
 sudo chmod 777 /dev/npheap
 sudo insmod kernel_module/tnpheap.ko
 sudo chmod 777 /dev/tnpheap
-./benchmark/benchmark 8192 8192 4
+./benchmark/benchmark 256 8192 4
 cat *.log > trace
 sort -n -k 3 trace > sorted_trace
-./benchmark/validate 8192 8192 < sorted_trace
+./benchmark/validate 256 8192 < sorted_trace
 rm -f *.log
 sudo rmmod tnpheap
 sudo rmmod npheap
