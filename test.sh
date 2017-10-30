442ef249339b2ff54a54bd32fdfba8262668a803
skavuru <skavuru@ncsu.edu>
2017-10-30 03:29:39 -0400

    All cases pass

--- test.sh
+++ test.sh
@@ -4,10 +4,10 @@ sudo insmod NPHeap/npheap.ko
 sudo chmod 777 /dev/npheap
 sudo insmod kernel_module/tnpheap.ko
 sudo chmod 777 /dev/tnpheap
-./benchmark/benchmark 256 8192 8
+./benchmark/benchmark 8192 8192 8
 cat *.log > trace
 sort -n -k 3 trace > sorted_trace
-./benchmark/validate 256 8192 < sorted_trace
+./benchmark/validate 8192 8192 < sorted_trace
 rm -f *.log
 sudo rmmod tnpheap
 sudo rmmod npheap
